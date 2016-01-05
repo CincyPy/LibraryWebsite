8ef608492c5d8a36804d66d0c3de57a140b37311
bmsauer <brianmsauer@gmail.com>
2016-01-04 19:48:34 -0500

    fixed tests, added admin tests

--- tests.py
+++ tests.py
@@ -27,18 +27,27 @@ class LibrarySiteTests(unittest.TestCase):
         
         #delete the test database
         try:
-            os.remove("test_library.db");
+            os.remove("test_library.db")
         except:
             pass
 
+    def login(self,u,p):
+         response = self.app.post('/login', data=dict(
+                username=u,
+                password=p,
+        ), follow_redirects=True)
+
+    def logout(self):
+        self.app.get('/logout',follow_redirects=True)
+
     def test_main(self):
         response = self.app.get('/')
-        self.assertIn("<h2>Welcome to the Library Site</h2>",response.data)
+        self.assertIn("Librarian Recommended",response.data)
         
     def test_login(self):
         #test initial get request
         response = self.app.get('/login')
-        self.assertIn("Welcome to the Staff Login for the Library Site",response.data)
+        self.assertIn("Welcome to the Staff Login",response.data)
         
         #test invalid user
         response = self.app.post('/login', data=dict(
@@ -59,7 +68,7 @@ class LibrarySiteTests(unittest.TestCase):
                 username="fred",
                 password="fred",
         ), follow_redirects=True) 
-        self.assertIn('Welcome to the Staff Page for the Library Site', response.data)
+        self.assertIn('Welcome to the Librarian Staff Page', response.data)
 
         
 
@@ -68,7 +77,24 @@ class LibrarySiteTests(unittest.TestCase):
             response = self.app.get('/logout',follow_redirects=True)
             self.assertIn('You were logged out',response.data)  
             self.assertNotIn("logged_in",flask.session)
+
+    def test_admin(self):
+        self.login("admin","admin")
+        #test admin
+        response = self.app.get("/admin")
+        self.assertIn("Add a new staff member:",response.data)
+        self.assertIn("/edit-profile/admin",response.data)
+        self.assertNotIn("/edit-profile/fred",response.data)
+
+        self.logout()
         
+        #test normal user
+        self.login("fred","fred")
+        response = self.app.get("/admin")
+        self.assertNotIn("Add a new staff member:",response.data)
+        self.assertIn("/edit-profile/fred",response.data)
+        self.assertIn("/profile/admin",response.data)
+        self.logout()
 
 if __name__ == '__main__':
     unittest.main()
