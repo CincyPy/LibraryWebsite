eef5415827ec51a4ae29d63c965c2f3323a54468
John <stamates@hotmail.com>
2016-02-01 18:33:19 -0500

    Cleaned up global variable definition for flask-mail variables

--- library.py
+++ library.py
@@ -13,19 +13,15 @@ from flask_mail import Mail, Message
 # configuration
 DATABASE = 'library.db'
 SECRET_KEY = '\x00\xb47\xb1\x1b<*tx\x1b2ywW\x86\x01\xfa\xcd\x0b\xeb\x94\x1c\xe5\xaf'
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_USERNAME = "KentonCountyLibrary@gmail.com"
+MAIL_PASSWORD = "CincyPyCoders"
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_DEFAULT_SENDER = "KentonCountyLibrary@gmail.com"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-app.config.update(dict(
-    MAIL_SERVER = "smtp.gmail.com",
-    MAIL_USERNAME = "KentonCountyLibrary@gmail.com",
-    MAIL_PASSWORD = "CincyPyCoders",
-    MAIL_PORT = 587,
-    MAIL_USE_TLS = True,
-    MAIL_DEFAULT_SENDER = "KentonCountyLibrary@gmail.com",
-))
-
 mail = Mail(app)
 
 
