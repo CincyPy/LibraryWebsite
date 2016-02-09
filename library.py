a979fbbc1aeced4e12792824427c3b4bfd46f913
Cincy Py <cincypy@gmail.com>
2016-02-09 12:33:31 -0500

    added publish route with dummy text

--- library.py
+++ library.py
@@ -329,5 +329,9 @@ def contact(uname):
         mail.send(msg)
         return redirect(url_for('profile', uname=uname))
 
+@app.route('/publish')
+def publish():
+    return "wat"
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=3000)
