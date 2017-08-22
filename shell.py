10a6b66c1bf2b232474eefa6c8f1e5864c528715
Carl Harris <elgoogemail2007@gmail.com>
2017-08-21 22:06:55 -0400

    better shell that you don't have to remember to run as

--- shell.py
+++ shell.py
@@ -1,11 +1,22 @@
-"""
-This script is how you can create an interactive shell within the app
-context.  It allows you to work on the db using SQLAlchemy commands
-instead of raw SQL.  To invoke, use 'python -i shell.py'
-"""
+import sqlalchemy as sqla
 from sqlalchemy import inspect
 
 from models import *
 from library import *
 
 app.app_context().push()
+
+context = locals()
+
+import argparse
+parser = argparse.ArgumentParser(description='Interactive shell preloaded with interesting objects of the app.')
+args = parser.parse_args()
+
+from config import sitename_from_readme
+
+appname = sitename_from_readme()
+if appname is None:
+    appname = 'App'
+
+import code
+code.interact(local=context, banner='%s interactive shell.' % appname)
